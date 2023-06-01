ed6d3407f5b1996d3fdc0d60e4e8096968193301
lananhgd <133091558+lananhgd@users.noreply.github.com>
2023-06-01 09:06:36 +0700

    Update ex1.php

--- ex1.php
+++ ex1.php
@@ -1,13 +1,1 @@
-<?php
-/*Bài 1: Viết chương trình PHP, sử dụng câu lệnh điều kiện if else để kiểm tra 1 số là số chẵn hay số lẻ?*/
-    function checkEvenOdd($number){
-     if($number%2==0){
-        return "Number $number is even.";
-     }else {
-        return"number $number is odd.";
-     }
-    }
-    echo checkEvenOdd(8);
-    echo"<br>";
-    echo checkEvenOdd(9);
-?>
+
